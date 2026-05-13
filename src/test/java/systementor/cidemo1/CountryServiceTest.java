82ff0972cd02b8bbaa419445e1b08bd898802c3c
smithasreelathamohan <smitha.s11may@gmail.com>
2026-05-14 01:09:07 +0200

    added unit tests

--- src/test/java/systementor/cidemo1/CountryServiceTest.java
+++ src/test/java/systementor/cidemo1/CountryServiceTest.java
@@ -1,17 +1,102 @@
 package systementor.cidemo1;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class CountryServiceTest {
 
-    @Test
-    void getSortedLaungagesReturnsAlphabeticallySortedLangues() {
-        when(countryApiClient.fetchCountryByName("Latvia"))
-            .thenReturn(LATVIA);
+        @Mock
+        CountryApiClient countryApiClient;
 
-        var result = countryService.getSortedBorders("Latvia");
+        @InjectMocks
+        CountryService countryService;
 
-        assertEquals(List.of("English", "Latvian"), result);
-    }
+        private static final Country LATVIA = new Country(
+                        "Latvia",
+                        "Republic of Latvia",
+                        "Latvia",
+                        "Latvian Republic",
+                        "Riga",
+                        "Europe",
+                        "Northern Europe",
+                        Map.of(
+                                        "lav", "Latvian",
+                                        "eng", "English"),
+                        List.of("EST", "LTU", "RUS", "BLR"),
+                        1829000,
+                        64559.0,
+                        "https://goo.gl/maps/iQpUkH7ghq31ZtXe9");
 
+        @Test
+        void getSortedLanguagesReturnsAlphabeticallySortedLanguages() {
+                when(countryApiClient.fetchCountryByName("Latvia"))
+                                .thenReturn(LATVIA);
+
+                var result = countryService.getSortedLanguages("Latvia");
+
+                assertEquals(List.of("English", "Latvian"), result);
+        }
+
+        @Test
+        void getSortedBordersReturnsAlphabeticallySortedBorders() {
+                when(countryApiClient.fetchCountryByName("Latvia"))
+                                .thenReturn(LATVIA);
+
+                var result = countryService.getSortedBorders("Latvia");
+
+                assertEquals(List.of("BLR", "EST", "LTU", "RUS"), result);
+        }
+
+        @Test
+        void getRegionInfoReturnsRegionAndSubregion() {
+                when(countryApiClient.fetchCountryByName("Latvia"))
+                                .thenReturn(LATVIA);
+
+                var result = countryService.getRegionInfo("Latvia");
+
+                assertEquals("Europe / Northern Europe", result);
+        }
+
+        @Test
+        void isHighlyPopulatedReturnsFalseForPopulationLessThan10Million() {
+                when(countryApiClient.fetchCountryByName("Latvia"))
+                                .thenReturn(LATVIA);
+
+                var result = countryService.isHighlyPopulated("Latvia");
+
+                assertFalse(result);
+        }
+
+        @Test
+        void getPopulationDensityReturnsCorrectDensity() {
+                when(countryApiClient.fetchCountryByName("Latvia"))
+                                .thenReturn(LATVIA);
+
+                var result = countryService.getPopulationDensity("Latvia");
+
+                assertEquals(28.33, result, 0.01);
+        }
+
+        @Test
+        void getGoogleMapsUrlReturnsCorrectUrl() {
+                when(countryApiClient.fetchCountryByName("Latvia"))
+                                .thenReturn(LATVIA);
+
+                var result = countryService.getGoogleMapsUrl("Latvia");
+
+                assertEquals("https://goo.gl/maps/iQpUkH7ghq31ZtXe9", result);
+        }
 }
